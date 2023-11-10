9d19a633fcea93618f45ff3529fd58df6caa3e93
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-11-10 16:44:27 -0300

    listarCuartelesOk

--- src/Vistas/ListarCuarteles.java
+++ src/Vistas/ListarCuarteles.java
@@ -2,21 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
 package Vistas;
 
-/**
- *
- * @author Julian Rios
- */
+import AccesoADatos.BomberoData;
+import AccesoADatos.CuartelData;
+import Entidades.Bombero;
+import Entidades.Cuartel;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
 public class ListarCuarteles extends javax.swing.JFrame {
 
-    
     public ListarCuarteles() {
         initComponents();
+        cargarComboBox();
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -27,19 +29,19 @@ public class ListarCuarteles extends javax.swing.JFrame {
         cmbCuartel = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        jtfNombre = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        jtfDireccion = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        jtfTelefono = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        jtfCorreo = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        jtfCoordenadaX = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
+        jtfCoordenadaY = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
-        jCheckBox1 = new javax.swing.JCheckBox();
+        jcbEstado = new javax.swing.JCheckBox();
         jLabel11 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -66,6 +68,11 @@ public class ListarCuarteles extends javax.swing.JFrame {
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 90, -1, -1));
 
         cmbCuartel.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        cmbCuartel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbCuartelActionPerformed(evt);
+            }
+        });
         getContentPane().add(cmbCuartel, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 90, 226, -1));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 10)); // NOI18N
@@ -76,51 +83,51 @@ public class ListarCuarteles extends javax.swing.JFrame {
         jLabel4.setText("Nombre:");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 163, -1, -1));
 
-        jTextField1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 165, 324, -1));
+        jtfNombre.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        getContentPane().add(jtfNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 165, 324, -1));
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
         jLabel5.setText("Dirección:");
         getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 205, -1, -1));
 
-        jTextField2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 207, 324, -1));
+        jtfDireccion.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        getContentPane().add(jtfDireccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 207, 324, -1));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
         jLabel6.setText("Teléfono:");
         getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 247, -1, -1));
 
-        jTextField3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        getContentPane().add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 249, 324, -1));
+        jtfTelefono.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        getContentPane().add(jtfTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 249, 324, -1));
 
         jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
         jLabel7.setText("Correo:");
         getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 289, -1, -1));
 
-        jTextField4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        getContentPane().add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 291, 324, -1));
+        jtfCorreo.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        getContentPane().add(jtfCorreo, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 291, 324, -1));
 
         jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
         jLabel8.setText("Coord X:");
         getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 331, -1, -1));
 
-        jTextField5.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        getContentPane().add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(308, 333, 320, -1));
+        jtfCoordenadaX.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        getContentPane().add(jtfCoordenadaX, new org.netbeans.lib.awtextra.AbsoluteConstraints(308, 333, 320, -1));
 
         jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
         jLabel9.setText("Coord Y:");
         getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(209, 373, -1, -1));
 
-        jTextField6.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        getContentPane().add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 375, 324, -1));
+        jtfCoordenadaY.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        getContentPane().add(jtfCoordenadaY, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 375, 324, -1));
 
         jLabel10.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
         jLabel10.setText("Estado:");
         getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(304, 419, -1, -1));
 
-        jCheckBox1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jCheckBox1.setText("ACTIVO / NO ACTIVO");
-        getContentPane().add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(377, 417, -1, 30));
+        jcbEstado.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jcbEstado.setText("ACTIVO / NO ACTIVO");
+        getContentPane().add(jcbEstado, new org.netbeans.lib.awtextra.AbsoluteConstraints(377, 417, -1, 30));
 
         jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fondo rojo.jpeg"))); // NOI18N
         getContentPane().add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 600));
@@ -132,12 +139,49 @@ public class ListarCuarteles extends javax.swing.JFrame {
         setVisible(false);
     }//GEN-LAST:event_btnCerraActionPerformed
 
-   
+    private void cmbCuartelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbCuartelActionPerformed
+
+        CuartelData cd = new CuartelData();
+        String cuartelNombre = (String) cmbCuartel.getSelectedItem();
+        if (cuartelNombre != "Cuarteles") {
+            Cuartel cuartel = new Cuartel();
+            cuartel = cd.BuscarCuartelPorNombre(cuartelNombre);
+
+            jtfNombre.setText(cuartel.getNombre_cuartel());
+            jtfDireccion.setText(cuartel.getDireccion());
+            jtfTelefono.setText(cuartel.getTelefono());
+            jtfCorreo.setText(cuartel.getCorreo());
+
+            //parseo double a string
+            String coorXstring = Double.toString(cuartel.getCoord_X());
+            String coorYstring = Double.toString(cuartel.getCoord_Y());
+
+            jtfCoordenadaX.setText(coorXstring);
+            jtfCoordenadaY.setText(coorYstring);
+            if (cuartel.isEstado() == true) {
+                jcbEstado.setSelected(true);
+            } else {
+                jcbEstado.setSelected(false);
+            }
+        }
+    }//GEN-LAST:event_cmbCuartelActionPerformed
+
+    //Cargar comboBox con cuarteles actuales 
+    private void cargarComboBox() {
+        cmbCuartel.removeAllItems();
+        Cuartel cuartelVacio = new Cuartel();
+        cuartelVacio.setNombre_cuartel("Cuarteles");
+        cmbCuartel.addItem(cuartelVacio.getNombre_cuartel());
+        CuartelData cd = new CuartelData();
+        List<Cuartel> list = cd.ListarCuarteles();
+        for (Cuartel elem : list) {
+            cmbCuartel.addItem(elem.getNombre_cuartel());
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCerra;
     private javax.swing.JComboBox<String> cmbCuartel;
-    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
@@ -149,12 +193,13 @@ public class ListarCuarteles extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
+    private javax.swing.JCheckBox jcbEstado;
+    private javax.swing.JTextField jtfCoordenadaX;
+    private javax.swing.JTextField jtfCoordenadaY;
+    private javax.swing.JTextField jtfCorreo;
+    private javax.swing.JTextField jtfDireccion;
+    private javax.swing.JTextField jtfNombre;
+    private javax.swing.JTextField jtfTelefono;
     // End of variables declaration//GEN-END:variables
 
 }
